e31f49a1122857d111a1bd2bc607b9c4f80dfdcb
TalibIOS <goldsikkait13@gmail.com>
2023-05-31 16:33:41 +0530

    ds

--- njerg/ViewController.swift
+++ njerg/ViewController.swift
@@ -8,12 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var baseView = AMOrderTrackerForUITableView()
+    @IBOutlet weak var tblView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tblView.delegate = self
+        tblView.dataSource = self
+        self.baseView.strip_color = .gray
+        self.baseView.setView(tblView: self.tblView, selected_indexForAnimation : 3)
     }
 
-
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
 }
 
+extension ViewController : UITableViewDataSource , UITableViewDelegate{
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 7
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "    Aiyub is Checking"
+        return cell
+    }
+}
